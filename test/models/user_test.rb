191e9ef2e23dec8bd7965159ba75f35d5f2e628a
YutaYokoi <dezirus.novajn.iojn@gmail.com>
2016-11-05 08:51:58 +0000

    6-26

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -50,6 +50,7 @@ class UserTest < ActiveSupport::TestCase
   
   test "email addresses should be unique" do
     duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
   end
