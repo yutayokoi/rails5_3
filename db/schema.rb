91e239be6401b2d07ee7d30d50790d7d4850d0a3
YutaYokoi <dezirus.novajn.iojn@gmail.com>
2016-11-05 08:55:38 +0000

    6-29

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161105072730) do
+ActiveRecord::Schema.define(version: 20161105085444) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
