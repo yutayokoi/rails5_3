13ed096cfdf7ed4919668ac00acae0e99fde73f9
YutaYokoi <dezirus.novajn.iojn@gmail.com>
2016-11-06 02:44:13 +0000

    7-19

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,11 +9,18 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])    # Not the final implementation!
+    @user = User.new(user_params)
     if @user.save
       # 保存の成功をここで扱う。
     else
       render 'new'
     end
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
