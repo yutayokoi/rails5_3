a4fcc57f83b9c4eeb702f8a640ad007b2a6c813a
YutaYokoi <dezirus.novajn.iojn@gmail.com>
2016-11-06 09:24:20 +0000

    9-19

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,8 +29,10 @@ class User < ApplicationRecord
 
   # 与えられたトークンがダイジェストと一致したらtrueを返す
   def authenticated?(remember_token)
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
+
 
   # ユーザーのログイン情報を破棄する
   def forget
