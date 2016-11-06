01900dd4c86cfebb2de5fba2a4ff3b3e6e86a9ee
YutaYokoi <dezirus.novajn.iojn@gmail.com>
2016-11-06 06:56:52 +0000

    8-27

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,28 @@
 class UsersController < ApplicationController
 
-  # => app/views/users/show.html.erb
   def show
     @user = User.find(params[:id])
-    #debugger
   end
-  
+
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
-      # success
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
-      # failure
       render 'new'
     end
   end
 
-  def user_params
-    params.require(:user).permit(
-      :name, :email, :password,
-      :password_confirmation)
-  end
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
