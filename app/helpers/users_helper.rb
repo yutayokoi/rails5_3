51b2c898b8c68d8f0706dbb5a7fe7b59fb990791
YutaYokoi <dezirus.novajn.iojn@gmail.com>
2016-11-12 02:59:13 +0000

    10-38

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
 
-  # 引数で与えられたユーザーのGravatar画像を返す
+  # 渡されたユーザーのGravatar画像を返す
   def gravatar_for(user, options = { size: 80 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
