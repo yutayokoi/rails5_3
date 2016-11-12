59ae1b9c1326ddefc459110442c9ecd32574bf7a
YutaYokoi <dezirus.novajn.iojn@gmail.com>
2016-11-12 01:48:04 +0000

    10-4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,11 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+  
+  def update
+    @user = User.find(params[:id])
+    render 'edit'
+  end
 
   private
 
