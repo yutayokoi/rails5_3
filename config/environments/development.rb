81c44140db1b208f90f85603ee0a9b7e3418d571
YutaYokoi <dezirus.novajn.iojn@gmail.com>
2016-11-12 06:40:03 +0000

    11-16

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,8 +28,12 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = 'rails5-yutayokoi.c9users.io'
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
