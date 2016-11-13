1fb787a1cb9d9a7f5aca1c4c79ef0b64ba20c4a2
YutaYokoi <dezirus.novajn.iojn@gmail.com>
2016-11-13 02:44:35 +0000

    13-34

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,24 +54,15 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
 
-    # beforeアクション
+    # beforeフィルター
 
-    # ログイン済みユーザーかどうか確認
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
-    
-    # 正しいユーザーかどうか確認
+    # 正しいユーザーかどうかを確認
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
     end
-    
-    # 管理者かどうか確認
+
+    # 管理者かどうかを確認
     def admin_user
       redirect_to(root_url) unless current_user.admin?
     end
