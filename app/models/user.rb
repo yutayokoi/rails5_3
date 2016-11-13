4e659a772aeb2dfb4ee7106104319b55887921c4
YutaYokoi <dezirus.novajn.iojn@gmail.com>
2016-11-13 08:57:49 +0000

    14-44

--- app/models/user.rb
+++ app/models/user.rb
@@ -77,10 +77,9 @@ class User < ApplicationRecord
     reset_sent_at < 2.hours.ago
   end
 
-  # 試作feedの定義
-  # 完全な実装は次章の「ユーザーをフォローする」を参照
+  # ユーザーのステータスフィードを返す
   def feed
-    Micropost.where("user_id = ?", id)
+    Micropost.where("user_id IN (?) OR user_id = ?", following_ids, id)
   end
 
   # ユーザーをフォローする
