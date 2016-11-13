f22c61f9d35aff8054dcade2ced69786cfc2533b
YutaYokoi <dezirus.novajn.iojn@gmail.com>
2016-11-13 06:33:52 +0000

    14-10

--- app/models/user.rb
+++ app/models/user.rb
@@ -79,7 +79,22 @@ class User < ApplicationRecord
     Micropost.where("user_id = ?", id)
   end
 
-    private
+  # ユーザーをフォローする
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  # ユーザーをアンフォローする
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしてたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
+  private
 
     # メールアドレスをすべて小文字にする
     def downcase_email
