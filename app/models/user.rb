b4c5f75cc1e7d094defabc50514385c5ae2bb7e7
YutaYokoi <dezirus.novajn.iojn@gmail.com>
2016-11-13 09:00:40 +0000

    14-47

--- app/models/user.rb
+++ app/models/user.rb
@@ -79,7 +79,10 @@ class User < ApplicationRecord
 
   # ユーザーのステータスフィードを返す
   def feed
-    Micropost.where("user_id IN (?) OR user_id = ?", following_ids, id)
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
   end
 
   # ユーザーをフォローする
